ee9be2d9dccc02fb6ab3f3078d057ce5784738ff
Josh McPhail <2877188M@student.gla.ac.uk>
2025-03-04 07:00:42 +0000

    Changed Population Script

--- populate_album_rater.py
+++ populate_album_rater.py
@@ -1,11 +1,11 @@
 import os
 import datetime as dt
-from django.contrib.auth.models import User
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
-
 import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WAD2GroupProject.settings')
+
 django.setup()
 
+from django.contrib.auth.models import User
 from album_rater.models import Album, UserProfile, Comment
 
 def add_album(title, uploader, upload_date = dt.date.today, views = 0, genre = "unknown"):
@@ -30,8 +30,7 @@ def add_comment(text, user_profile, album, score = 0):
     return c
 
 def populate():
-    #First names i could think of
-    user_info = [("Josh", "McPhail"), ("Malachy", "McGinnity"), ("Pavlo", "Matveiev"), ("Declan", "McLaren")]
+    user_info = [("username0", "password"), ("username1", "password"), ("username2", "password"), ("username3", "password")]
     users = []
     for user in user_info:
         users.append(add_user_profile(user[0], user[1]))
@@ -41,7 +40,7 @@ def populate():
                     ("Last one, I swear", "unknown"), ("Kendrick Lamar's 2025 Superbowl Half-Time Show", "rap")]
     albums = []
     for i in range(len(album_titles)):
-        albums.append(add_album(album_titles[i][0], users[i%4], genre = album_titles[i][0]))
+        albums.append(add_album(album_titles[i][0], users[i%4], genre = album_titles[i][1]))
     
     for i in range(len(users)):
         users[i].favourite_album = albums[(2*i + 3)%len(albums)]
@@ -52,6 +51,8 @@ def populate():
 
         users[i].users_followed.add(users[(3*i + 7)%len(users)])
         users[i].users_followed.add(users[(4*i - 2)%len(users)])
+
+        users[i].save()
     
     user_messages = ["Great Job", "kinda mid", "According to all known laws of aviation...", "A MINOOOOOOOOR", "Honestly, it sucks",
                      "Gotta love this artists work", "Did you know that Barack Obama is (as of 2025-03-03) the only US President to serve there term under the same flag they were born under?",
@@ -59,7 +60,7 @@ def populate():
     comments = []
     for i in range(len(users)):
         for j in range(len(users)):
-            comments.append(user_messages[(len(users)*i + j)%len(user_messages)], users[i], albums[j])
+            comments.append(add_comment(user_messages[(len(users)*i + j)%len(user_messages)], users[i], albums[j]))
 
 if __name__ == "__main__":
     print("Starting Album Rater popultaion script...")
