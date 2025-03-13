84367a4e54cec072641a9e66ac0a438f35c37aed
Pavlo Matveiev <matveievp@gmail.com>
2025-03-13 08:51:33 +0000

    Change urls.py for the app, Add AJAX links

--- album_rater/urls.py
+++ album_rater/urls.py
@@ -4,15 +4,19 @@ from album_rater import views
 app_name = "album-rater"
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('about/',views.about,name="about"),
-    path('sign_up/',views.register,name="register"),
-    path('log_in/',views.user_login,name="log_in"),
-    path('log_out/',views.user_logout,name="log_out"),
-    path('upload/',views.upload,name="upload"),
-    path('delete_account/',views.delete_account,name="delete_account"),
-    path('profile/<slug:username_slug>',views.profile,name="user_profile"),
-    path('album/<slug:album_name>',views.album,name="album"),
-    path('charts/',views.charts,name="charts"),
-    path('change_password',views.change_password,name="change_password"),
+    path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('accounts/register/', views.account_register, name="register"),
+    path('accounts/login/', views.account_login, name="login"),
+    path('accounts/logout/', views.account_logout, name="logout"),
+    path('accounts/password-change/', views.account_password_change, name="password_change"),
+    path('accounts/delete-account/', views.account_delete, name="delete_account"),
+    path('user/<str:username>/', views.profile, name="user_profile"),
+    path('album/<slug:album_slug>/', views.album, name="album_detail"),
+    path('album/create/', views.album_create, name="album_create"),
+    path('search/', views.search, name="search"),
+    # AJAX endpoints
+    path('ajax/rate_album/', views.ajax_rate_album, name="ajax_rate_album"),
+    path('ajax/follow_user/', views.ajax_follow_user, name="ajax_follow_user"),
+    path('ajax/comment_album/', views.ajax_comment_album, name="ajax_comment_album"),
 ]
